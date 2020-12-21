e0872b85f080d6de56a1996f91ce1940de358696
Batmancheat <75727528+Batmancheat@users.noreply.github.com>
2020-12-21 14:31:43 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzQyMzUzMjIzNzgzODA5MDc1.XzE4UA.29PmWVr2lZKl1C1ChuTuzgkcAj0", ///  تۆکینی بۆت
+  YT_API_KEY: "api ", /// مش مهم
+  prefix: "D", // کۆماندی بۆتەکە دانێ
+  devs: ["497040442060439582"] ///// ئایدی خۆت
+};
